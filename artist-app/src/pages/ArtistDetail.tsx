e7e4595d65763859d053d60c6567f9382d16918d
Laurent <laurent_baj@hotmail.com>
2021-11-04 23:37:48 +0100

    Feature finished

--- artist-app/src/pages/ArtistDetail.tsx
+++ artist-app/src/pages/ArtistDetail.tsx
@@ -1,4 +1,4 @@
-import { FC, useContext, useState } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ArtistItem from "../components/Artist/ArtistItem";
 import { ArtistContext } from "../contexts/ArtistContext";
@@ -9,12 +9,12 @@ import { ArtistContextType } from "../types/ArtistContextType";
 export const ArtistDetail: FC = () => {
   const {name} = useParams<{ name: string }>();
   const {artists} = useContext(ArtistContext) as ArtistContextType
-  
-  
+  const artist = artists.find(a => a.name === name)
 
   return (
     <>
-      <h1>{name}</h1>
+      <h1>{artist?.name}</h1> 
+      <p>{artist?.description}</p>    
     </>
   );
 };
