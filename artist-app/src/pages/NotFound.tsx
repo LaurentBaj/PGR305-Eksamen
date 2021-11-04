74d033c194beac7f751cbed4ebfb4bb2783f2dcd
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-04 17:33:02 +0000

    add 404 not found page

---
+++ artist-app/src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+export const NotFound: FC = () => {
+  return (
+    <div className="align-items-center justify-content-center">
+      <h1>404 - Not Found</h1>
+      {/* <Link to="/">Go home</Link> */}
+    </div>
+  );
+};
