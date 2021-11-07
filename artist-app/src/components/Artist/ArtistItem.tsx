f2b6cc471b47e485e5bfc4873e04063a2807ff64
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-07 17:39:27 +0000

    edit card (removed spacing) and shadows

--- artist-app/src/components/Artist/ArtistItem.tsx
+++ artist-app/src/components/Artist/ArtistItem.tsx
@@ -1,20 +1,26 @@
 import { FC } from "react";
 import { Card } from "react-bootstrap";
 import { IArtist } from "../../interfaces/IArtist";
+import styles from "../../styles/card-styles.module.css";
 
 const ArtistItem: FC<IArtist> = ({ id, name, image, description }) => {
   return (
-    <Card>
+    <Card className={styles.layeredbox}>
+      {image && (
+        <Card.Img
+          variant="top"
+          alt=""
+          src={`https://localhost:5001/images/${image}`}
+        />
+      )}
+      {!image && (
+        <Card.Img
+          variant="top"
+          alt=""
+          src={`https://localhost:5001/images/user_placeholder.png`}
+        />
+      )}
       <Card.Body>
-        {image && (
-          <Card.Img alt="" src={`https://localhost:5001/images/${image}`} />
-        )}
-        {!image && (
-          <Card.Img
-            alt=""
-            src={`https://localhost:5001/images/user_placeholder.png`}
-          />
-        )}
         <Card.Title className={"text-center pt-3"}>{name}</Card.Title>
       </Card.Body>
     </Card>
