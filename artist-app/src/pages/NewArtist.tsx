19ba11389bc4550720b08bc8a75b411cebec9ae9
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-08 12:57:24 +0000

    create form component

--- artist-app/src/pages/NewArtist.tsx
+++ artist-app/src/pages/NewArtist.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
+import ArtistForm from "../components/shared/ArtistForm";
 
 const NewArtist: FC = () => {
-  return <h1>New Artist</h1>;
+  return (
+    <>
+      <h1>New Artist</h1>
+      <ArtistForm />
+    </>
+  );
 };
 
 export default NewArtist;
