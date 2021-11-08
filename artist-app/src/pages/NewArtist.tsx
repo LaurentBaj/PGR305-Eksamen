d78791d16a235f786fbabe66f9984bb3cf2aec97
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-08 12:42:50 +0000

    add routing for new artist page

---
+++ artist-app/src/pages/NewArtist.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const NewArtist: FC = () => {
+  return <h1>New Artist</h1>;
+};
+
+export default NewArtist;
--- artist-app/src/routing/Routing.tsx
+++ artist-app/src/routing/Routing.tsx
@@ -4,10 +4,11 @@ import { Container } from "react-bootstrap";
 import Home from "../pages/Home";
 import Artist from "../pages/Artist";
 import About from "../pages/About";
-import {ArtistView} from "../pages/ArtistView"
+import { ArtistView } from "../pages/ArtistView";
 import NavigationBar from "../components/shared/NavigationBar";
 import { NotFound } from "../pages/NotFound";
-import {ArtistProvider} from "../contexts/ArtistContext";
+import { ArtistProvider } from "../contexts/ArtistContext";
+import NewArtist from "../pages/NewArtist";
 
 const Routing: FC = () => {
   return (
@@ -15,13 +16,14 @@ const Routing: FC = () => {
       <NavigationBar />
       <Container>
         <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/artists" component={Artist} />
-            <Route path="/about" component={About} />
-            <ArtistProvider>
-                <Route path={`/artists/:name`} component={ArtistView} />
-            </ArtistProvider>
-            <Route component={NotFound} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/artists" component={Artist} />
+          <Route path="/about" component={About} />
+          <Route path="/newArtist" component={NewArtist} />
+          <ArtistProvider>
+            <Route path={`/artists/:name`} component={ArtistView} />
+          </ArtistProvider>
+          <Route component={NotFound} />
         </Switch>
       </Container>
     </BrowserRouter>
