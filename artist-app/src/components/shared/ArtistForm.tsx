9a80b78aec7074251372631adb2e19345876dfef
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-08 14:22:45 +0000

    add button to newartist

--- artist-app/src/components/shared/ArtistForm.tsx
+++ artist-app/src/components/shared/ArtistForm.tsx
@@ -1,35 +1,35 @@
 import { FC, useState } from "react";
-import { Form, Row, Col} from "react-bootstrap";
+import { Form, Row, Col } from "react-bootstrap";
 import { IArtist } from "../../interfaces/IArtist";
 import { Genre } from "./Genre";
 
-const ArtistForm: FC= () => {
+const ArtistForm: FC = () => {
   return (
     <>
       <h1>New Artist</h1>
       <Form>
-      <Form.Group className="mb-3" >
-        <Form.Label>Artist Name: </Form.Label>
-        <Form.Control type="text"/>
-      </Form.Group>
-      <Form.Group className="mb-3" >
-        <Form.Label>Description</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      <br></br>
-      <Row>
-        <Col>
-          <Form.Select>
-            {Object.keys(Genre).map((i) => (
-              <option>{i}</option>
-            ))}
-        </Form.Select>
-        </Col>
-        <Col>
-          <Form.Control placeholder="Date of birth" />
-        </Col>
-      </Row>
-    </Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Artist Name: </Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Description</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <br></br>
+        <Row>
+          <Col>
+            <Form.Select>
+              {Object.keys(Genre).map((i) => (
+                <option>{i}</option>
+              ))}
+            </Form.Select>
+          </Col>
+          <Col>
+            <Form.Control placeholder="Date of birth" />
+          </Col>
+        </Row>
+      </Form>
     </>
   );
 };
--- artist-app/src/pages/Artist.tsx
+++ artist-app/src/pages/Artist.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import ArtistList from "../components/Artist/ArtistList";
 import { ArtistProvider } from "../contexts/ArtistContext";
+import { Button } from "react-bootstrap";
 
 const Artist: FC = () => {
   return (
     <>
       <h1>Artist</h1>
+      <Link to="/newartist">
+        <Button variant="primary">New Artist</Button>
+      </Link>
       <ArtistProvider>
         <ArtistList />
       </ArtistProvider>
