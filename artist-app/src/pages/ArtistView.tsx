853f12b0a54675b5d3b3586886b52b907c0c380c
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-10 13:23:42 +0000

    refactor image rendering and add styling

--- artist-app/src/pages/ArtistView.tsx
+++ artist-app/src/pages/ArtistView.tsx
@@ -2,39 +2,49 @@ import { FC, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ArtistContext } from "../contexts/ArtistContext";
 import { ArtistContextType } from "../types/ArtistContextType";
-import { Col, Container, Image, Row } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
+import styles from "../styles/card-styles.module.css";
 
 export const ArtistView: FC = () => {
   const { name } = useParams<{ name: string }>();
   const { artists } = useContext(ArtistContext) as ArtistContextType;
   const [artist] = useState(artists.find((a) => a.name === name));
 
+  // check if artists contains a image
+  const containsImage = () => {
+    if (artist?.image) {
+      return (
+        <Image
+          className={styles.layeredbox}
+          style={{ maxWidth: "20rem" }}
+          src={`https://localhost:5001/images/${artist.image}`}
+        />
+      );
+    } else {
+      return (
+        <Image
+          style={{ maxWidth: "20rem" }}
+          src={`https://localhost:5001/images/user_placeholder.png`}
+        />
+      );
+    }
+  };
+
   return (
-    <Container>
+    <>
+      <Row className="m-5 text-center">
+        <Col>{containsImage()}</Col>
+      </Row>
       <Row>
         <Col>
-          {artist?.image && (
-            <Image
-              alt=""
-              style={{ maxWidth: "24rem" }}
-              src={`https://localhost:5001/images/${artist.image}`}
-            />
-          )}
-          {!artist?.image && (
-            <Image
-              alt=""
-              style={{ maxWidth: "24rem" }}
-              src={`https://localhost:5001/images/user_placeholder.png`}
-            />
-          )}
+          <h1>{artist?.name}</h1>
         </Col>
       </Row>
       <Row>
-        <h1>{artist?.name}</h1>
+        <Col>
+          <p>{artist?.description}</p>
+        </Col>
       </Row>
-      <Row>
-        <p>{artist?.description}</p>
-      </Row>
-    </Container>
+    </>
   );
 };
