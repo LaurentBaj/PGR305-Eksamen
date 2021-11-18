e98196cab5eafc88f574b4284549b7a1a80e142e
Laurent <laurent_baj@hotmail.com>
2021-11-18 10:20:33 +0100

    new artist form handles everything but genre

--- artist-app/src/components/Artist/NewArtistForm.tsx
+++ artist-app/src/components/Artist/NewArtistForm.tsx
@@ -1,13 +1,14 @@
 import { FC, ChangeEvent, useState } from "react";
+import { Form } from "react-bootstrap";
 import { IArtist } from "../../interfaces/IArtist";
 import { ArtistService } from "../../services/ArtistService";
 import { Genre } from "../shared/Genre";
-
 
 export const NewArtistForm: FC = () => {
 
     const [artist, setNewArtist] = useState<IArtist>({ name: "", description: "", image: "", genre: Genre.Pop, dateOfBirth: "" })
     const [newImage, setNewImage] = useState<File>()
+    const [genre, setGenre] = useState<Genre>(Genre.Pop)
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         let { name } = event.target
@@ -29,6 +30,9 @@ export const NewArtistForm: FC = () => {
             case "description":
                 setNewArtist({ ...artist, description: value })
                 break;
+            case "date":
+                setNewArtist({ ...artist, dateOfBirth: value })
+                break;
         }
     }
 
@@ -49,6 +53,20 @@ export const NewArtistForm: FC = () => {
             <div>
                 <label>Description</label>
                 <input name="description" onChange={handleChange} type="text" />
+            </div>
+            <div>
+                <Form.Select value={genre} onChange={e => setGenre(e.target.value as Genre)}>
+                    {Object.keys(Genre).map((i) => (
+                        <option>{i}</option>
+                    ))}
+                </Form.Select>
+            </div>
+            <div>
+                <Form.Group className="mb-3">
+                    <Form.Control
+                        name="date" onChange={handleChange} type="text"
+                        placeholder={"Date of birth - ( dd/mm/yyyy )"} />
+                </Form.Group>
             </div>
             <input onClick={postNewArtist} type="button" value="Add artist" />
         </section>
