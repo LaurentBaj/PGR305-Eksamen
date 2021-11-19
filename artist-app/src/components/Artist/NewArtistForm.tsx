f56bffe5ffa00fc919269f7fcbc3f44db821e099
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-19 13:10:45 +0000

    edit new artist form

--- artist-app/src/components/Artist/NewArtistForm.tsx
+++ artist-app/src/components/Artist/NewArtistForm.tsx
@@ -46,19 +46,16 @@ export const NewArtistForm: FC = () => {
   return (
     <Form>
       <Row>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>Artist Name: </Form.Label>
-            <Form.Control name="name" onChange={handleChange} type="text" />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label>Add image: </Form.Label>
-            <Form.Control name="image" onChange={handleChange} type="file" />
-          </Form.Group>
-        </Col>
+        <Form.Group as={Col} className="mb-3">
+          <Form.Label>Artist Name: </Form.Label>
+          <Form.Control name="name" onChange={handleChange} type="text" />
+        </Form.Group>
+        <Form.Group as={Col} className="mb-3">
+          <Form.Label>Add image: </Form.Label>
+          <Form.Control name="image" onChange={handleChange} type="file" />
+        </Form.Group>
       </Row>
+
       <Row>
         <Form.Group className="mb-3">
           <Form.Label>Description: </Form.Label>
@@ -69,22 +66,26 @@ export const NewArtistForm: FC = () => {
           />
         </Form.Group>
       </Row>
+
       <Row>
-        <Col>
-          <Form.Select onChange={() => handleChange}>
-            <option value="Classic">Classic</option>
-            <option value="Pop">Pop</option>
-          </Form.Select>
-        </Col>
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Control
-              name="date"
-              onChange={handleChange}
-              placeholder={"Date of birth - ( dd/mm/yyyy )"}
-            />
-          </Form.Group>
-        </Col>
+        <Form.Group as={Col} className="mb-3">
+          <Form.Label>Genre: </Form.Label>
+          <Form.Control
+            name="genre"
+            onChange={handleChange}
+            type="text"
+            placeholder="Rock, Pop, Classic..."
+          />
+        </Form.Group>
+        <Form.Group as={Col} className="mb-3">
+          <Form.Label>Date of Birth: </Form.Label>
+          <Form.Control
+            name="date"
+            onChange={handleChange}
+            type="text"
+            placeholder={"dd/mm/yyyy"}
+          />
+        </Form.Group>
       </Row>
       <Button onClick={postNewArtist}>Submit</Button>
     </Form>
