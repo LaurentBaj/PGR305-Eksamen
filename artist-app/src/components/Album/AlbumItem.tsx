4d4a0ac7ea3a04dcdf99a995ca5312c32c0d8c18
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-20 13:50:56 +0000

    modal on delete album

--- artist-app/src/components/Album/AlbumItem.tsx
+++ artist-app/src/components/Album/AlbumItem.tsx
@@ -1,19 +1,24 @@
-import { FC } from "react";
-import { Button } from "react-bootstrap";
+import { FC, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import { IAlbum } from "../../interfaces/IAlbum";
 import { AlbumService } from "../../services/AlbumService";
 import { TrashFill } from "react-bootstrap-icons";
 
-export const AlbumItem: FC<IAlbum> = ({ id, name, songs, artist_id }) => {
+export const AlbumItem: FC<IAlbum> = ({ id, name, songs }) => {
   const deleteAlbum = (id: string) => {
     AlbumService.deleteAlbum(id);
   };
+
+  // modal
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
       <h3>
         {name}
-        <Button onClick={() => deleteAlbum(id as string)} variant="text">
+        <Button onClick={handleShow} variant="text">
           <TrashFill color="red" />
         </Button>
       </h3>
@@ -23,6 +28,23 @@ export const AlbumItem: FC<IAlbum> = ({ id, name, songs, artist_id }) => {
           return <li key={key}>{s}</li>;
         })}
       </ul>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Deleting artist</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete the album <b>{name}</b>?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={() => deleteAlbum(id as string)}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
