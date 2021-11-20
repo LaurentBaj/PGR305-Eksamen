69e6e8a5d1fed76850fb27c04346f8bf35c83961
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-20 15:55:13 +0000

    added functionality

--- artist-app/src/pages/Artist.tsx
+++ artist-app/src/pages/Artist.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import ArtistList from "../components/Artist/ArtistList";
-import { ArtistProvider } from "../contexts/ArtistContext";
 import { Button } from "react-bootstrap";
 
 const Artist: FC = () => {
