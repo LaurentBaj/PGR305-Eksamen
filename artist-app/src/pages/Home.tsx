1b5efb13726a52af4f66cb3fec47624f1e02d029
Henrik Bråthen <hsbrathen1@gmail.com>
2021-11-21 10:31:42 +0000

    home styling

--- artist-app/src/pages/Home.tsx
+++ artist-app/src/pages/Home.tsx
@@ -1,7 +1,25 @@
 import { FC } from "react";
+import { Col, Image, Row } from "react-bootstrap";
+import image from "../assets/home-image.png";
 
 const Home: FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Row>
+        <Col className="text-center mt-5">
+          <h3>
+            Welcome to Artist<span style={{ color: "orange" }}>Hub</span>
+          </h3>
+          <p>Save all your favourite artists and their albums in one place</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <Image src={image} alt={image} fluid />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Home;
